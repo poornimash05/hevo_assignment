15d505efc32c5d0c216eef6bdfc4d8d7c084e0f5
Poornima Sharma <129762914+poornimash05@users.noreply.github.com>
2026-01-21 21:40:39 +0530

    Update 04_customers_transform.sql

--- assignment_01/sql/04_customers_transform.sql
+++ assignment_01/sql/04_customers_transform.sql
@@ -1,1 +1,8 @@
+CREATE OR REPLACE VIEW customers_clean AS
+SELECT
+    id,
+    LOWER(email) AS email,
+    SPLIT_PART(email, '@', 1) AS username,
+FROM customers;
 
+select * from pc_hevodata_db.public.customers_clean;
