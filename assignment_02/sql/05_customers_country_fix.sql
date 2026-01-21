2976c63982018b890e3bba848684e4b2a4d874ad
Poornima Sharma <129762914+poornimash05@users.noreply.github.com>
2026-01-21 16:11:11 +0530

    view and enhance country code logic

--- assignment_02/sql/05_customers_country_fix.sql
+++ assignment_02/sql/05_customers_country_fix.sql
@@ -1,16 +1,36 @@
-CREATE OR REPLACE TABLE PC_HEVODATA_DB.PUBLIC.CUSTOMERS_CLEAN_FINAL AS
+CREATE OR REPLACE VIEW PC_HEVODATA_DB.PUBLIC.CUSTOMERS_CLEAN_FINAL AS
 SELECT
     c.customer_id,
+
     c.email,
+
     c.phone,
-    COALESCE(cd.iso_code, 'UNKNOWN') AS country_code,
-    COALESCE(c.created_at, '1900-01-01'::TIMESTAMP) AS created_at
+
+    COALESCE(
+        cd.iso_code,
+        CASE
+            WHEN c.country_code IS NULL OR TRIM(c.country_code) = '' THEN NULL
+            ELSE 'UNKNOWN'
+        END
+    ) AS country_code,
+
+    COALESCE(
+        c.created_at,
+        TO_TIMESTAMP('1900-01-01 00:00:00')
+    ) AS created_at
 
 FROM PC_HEVODATA_DB.PUBLIC.CUSTOMERS_CLEAN c
 LEFT JOIN PC_HEVODATA_DB.PUBLIC.COUNTRY_DIM cd
     ON UPPER(TRIM(c.country_code)) IN (
         UPPER(cd.country_name),
-        UPPER(cd.iso_code)
+        UPPER(cd.iso_code),
+        CASE
+            WHEN cd.iso_code = 'US' THEN 'USA'
+            WHEN cd.iso_code = 'US' THEN 'UNITEDSTATES'
+            WHEN cd.iso_code = 'IN' THEN 'IND'
+            WHEN cd.iso_code = 'SG' THEN 'SINGAPORE'
+        END
     );
 
-SELECT * FROM PC_HEVODATA_DB.PUBLIC.CUSTOMERS_CLEAN_FINAL LIMIT 10;
+SELECT * 
+FROM PC_HEVODATA_DB.PUBLIC.CUSTOMERS_CLEAN_FINAL;
