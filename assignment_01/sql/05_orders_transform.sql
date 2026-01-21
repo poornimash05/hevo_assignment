bb1c9957ba86a98856e7bffbf58f49b1c9bd9b9a
Poornima Sharma <129762914+poornimash05@users.noreply.github.com>
2026-01-21 21:36:18 +0530

    Create view for order events with status mapping

--- assignment_01/sql/05_orders_transform.sql
+++ assignment_01/sql/05_orders_transform.sql
@@ -1,1 +1,15 @@
+CREATE OR REPLACE view pc_hevodata_db.public.order_events AS
+SELECT
+   ID AS ORDER_ID,
+   CASE
+       WHEN STATUS = 'placed' THEN 'order_placed'
+       WHEN STATUS = 'shipped' THEN 'order_shipped'
+       WHEN STATUS = 'delivered' THEN 'order_delivered'
+       WHEN STATUS = 'cancelled' THEN 'order_cancelled'
+       ELSE 'order_unknown'
+   END AS EVENT_TYPE,
+   CURRENT_TIMESTAMP() AS PROCESSED_AT
+FROM pc_hevodata_db.public.ORDERS;
+
+select * from pc_hevodata_db.public.order_events;
 
