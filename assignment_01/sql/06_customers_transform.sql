a8d4e5d8e2bcac3342509c9ff724f58eed71b7ca
Poornima Sharma <129762914+poornimash05@users.noreply.github.com>
2026-01-23 16:06:15 +0530

    Delete assignment_01/sql/06_customers_transform.sql

--- assignment_01/sql/06_customers_transform.sql
+++
@@ -1,8 +0,0 @@
-CREATE OR REPLACE VIEW customers_clean AS
-SELECThttps://github.com/poornimash05/hevo_assignment/edit/main/assignment_01/sql/04_customers_transform.sql
-    id,
-    LOWER(email) AS email,
-    SPLIT_PART(email, '@', 1) AS username,
-FROM customers;
-
-select * from pc_hevodata_db.public.customers_clean;
